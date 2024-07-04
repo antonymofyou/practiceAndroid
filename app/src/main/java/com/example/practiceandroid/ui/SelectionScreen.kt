29f44d9de324ae3de3dfaf78b8b8aeb0a7d975f3
lofinif <ivan.bagaley@gmail.com>
2024-07-04 16:58:13 +0300

    обновил экран с выбором ячеек

--- app/src/main/java/com/example/practiceandroid/ui/SelectionScreen.kt
+++ app/src/main/java/com/example/practiceandroid/ui/SelectionScreen.kt
@@ -1,12 +1,16 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,7 +18,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.practiceandroid.R
+import com.example.practiceandroid.viewModels.SelectionScreenViewModel
+import ru.nasotku.nasotkuapp.theme.components.NoLazyGrid
 
 enum class CellState {
     UNCHECKED, CHECKED, INTERMEDIATE
@@ -25,12 +32,14 @@ data class Cell(val id: Int, var state: CellState)
 
 @Composable
 fun GridCell(cell: Cell, onClick: () -> Unit) {
+    //выбираем цвет иконки
     val color = when (cell.state) {
         CellState.UNCHECKED -> Color.Red
         CellState.CHECKED -> Color.Green
         CellState.INTERMEDIATE -> Color(0xFFFFA500)
     }
 
+    //создаем ячейку
     Box(
         modifier = Modifier
             .padding(4.dp)
@@ -39,6 +48,7 @@ fun GridCell(cell: Cell, onClick: () -> Unit) {
             .clickable { onClick() },
         contentAlignment = Alignment.Center
     ) {
+        //добавляем иконку к ячейке
         Icon(painter = when (cell.state){
             CellState.UNCHECKED -> painterResource(R.drawable.ic_unchecked)
             CellState.CHECKED -> painterResource(R.drawable.ic_check_mark)
@@ -52,32 +62,38 @@ fun GridCell(cell: Cell, onClick: () -> Unit) {
 }
 
 @Composable
-fun GridView(cells: List<Cell>) {
-    var showPopup by remember { mutableStateOf(false) }
-    var selectedCell by remember { mutableStateOf<Cell?>(null) }
-
+fun GridView(cells: List<Cell>, viewModel: SelectionScreenViewModel = viewModel()) {
+    //добавляем список ячеек
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        LazyVerticalGrid(columns = GridCells.Fixed(5), modifier = Modifier.padding(16.dp)) {
-            items(cells) { cell ->
-                GridCell(cell) {
-                    selectedCell = cell
-                    showPopup = true
-                }
+        NoLazyGrid(
+            itemWidth = 68, // если выбрать другое значение, то отображается некорректно
+            itemCount = cells.size,
+            spaceHorizontal = 5,
+            spaceVertical = 5,
+            modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+        ) { index ->
+            GridCell(cells[index]) {
+                viewModel.onCellClick(cells[index]) //при нажатии показываем Popup
             }
         }
 
-        if (showPopup && selectedCell != null) {
-            Popup(alignment = Alignment.Center, onDismissRequest = { showPopup = false }) {
+        //если showPopup и selectedCell не null, то показываем Popup
+        if (viewModel.showPopup && viewModel.selectedCell != null) {
+            //создаем Popup
+            Popup(alignment = Alignment.Center, onDismissRequest = { viewModel.dismissPopup() }) {
                 Box(
                     modifier = Modifier
                         .border(1.dp, Color.Black)
                         .background(Color.White)
                         .padding(16.dp)
                 ) {
+                    //добавляем содержимое в Popup
                     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = "Урок ${selectedCell!!.id}")
+                        Text(text = "Урок ${viewModel.selectedCell!!.id}")
                         Spacer(modifier = Modifier.height(8.dp))
-                        Button(onClick = { showPopup = false }) {
+                        Button(onClick = { viewModel.dismissPopup() }) {
                             Text("Перейти")
                         }
                     }
@@ -86,6 +102,8 @@ fun GridView(cells: List<Cell>) {
         }
     }
 }
+
+
 
 @Preview
 @Composable
@@ -98,7 +116,14 @@ fun Preview() {
                 Cell(7, CellState.INTERMEDIATE), Cell(8, CellState.CHECKED),
                 Cell(9, CellState.UNCHECKED), Cell(10, CellState.CHECKED),
                 Cell(11, CellState.CHECKED), Cell(12, CellState.UNCHECKED),
-                Cell(13, CellState.UNCHECKED), Cell(14, CellState.INTERMEDIATE)
+                Cell(13, CellState.UNCHECKED), Cell(14, CellState.INTERMEDIATE),
+                Cell(7, CellState.INTERMEDIATE), Cell(8, CellState.CHECKED),
+                Cell(9, CellState.UNCHECKED), Cell(10, CellState.CHECKED),
+                Cell(11, CellState.CHECKED), Cell(12, CellState.UNCHECKED),
+                Cell(13, CellState.UNCHECKED), Cell(14, CellState.INTERMEDIATE),
+                Cell(15, CellState.INTERMEDIATE), Cell(16, CellState.INTERMEDIATE),
+                Cell(17, CellState.INTERMEDIATE), Cell(18, CellState.INTERMEDIATE),
+                Cell(19, CellState.INTERMEDIATE)
             )
         )
 }
