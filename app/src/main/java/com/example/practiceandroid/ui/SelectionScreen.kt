447633da601351e428af57bdc4a7648547061171
lofinif <ivan.bagaley@gmail.com>
2024-07-04 16:58:54 +0300

    обновил экран с выбором ячеек

--- app/src/main/java/com/example/practiceandroid/ui/SelectionScreen.kt
+++ app/src/main/java/com/example/practiceandroid/ui/SelectionScreen.kt
@@ -1,16 +1,11 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
