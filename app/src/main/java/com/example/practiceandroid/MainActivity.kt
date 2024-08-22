eea5d61cc376a686d3ede15352c888bd25fbe23d
Chelozzi <greyrata@mail.ru>
2024-08-22 16:59:14 +0500

    Сделал расширение с clipToBounds()

--- app/src/main/java/com/example/practiceandroid/MainActivity.kt
+++ app/src/main/java/com/example/practiceandroid/MainActivity.kt
@@ -4,46 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme.shapes
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.practiceandroid.models.ResponseShapes
 import com.example.practiceandroid.theme.PracticeAndroidTheme
 import com.example.practiceandroid.viewModels.MainViewModel
 import com.example.practiceandroid.views.DrawShapes
-import kotlin.math.cos
-import kotlin.math.sin
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,9 +24,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val mainViewModel: MainViewModel = viewModel()
-            PracticeAndroidTheme {
-                Box(
-                    modifier = Modifier.fillMaxSize()
+
+            PracticeAndroidTheme(
+            ) {
+
+                Box (
+                    modifier = Modifier.height(300.dp).width(300.dp)
                 ){
                     DrawShapes(mainViewModel.responseShapes.value)
                 }
--- app/src/main/java/com/example/practiceandroid/views/DrawShapes.kt
+++ app/src/main/java/com/example/practiceandroid/views/DrawShapes.kt
@@ -5,17 +5,24 @@ import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
 import com.example.practiceandroid.ext.decodeBase64ToBitmap
 import com.example.practiceandroid.models.ResponseShapes
 
@@ -31,24 +38,27 @@ fun DrawShapes(responseShapes: ResponseShapes) {
     var minOffsetY = 0f
 
     BoxWithConstraints (
+        // Ограничиваем расширение
+        modifier = Modifier.clipToBounds(),
     ){
         val localDensity = LocalDensity.current
         val state = rememberTransformableState { scaleChange, offsetChange, _ ->
 
-            // Проверяем, выходим ли за границы Root view
-            if ((maxWidth.value * (scale * scaleChange) <= maxWidth.value) &&
-                (maxHeight.value * (scale * scaleChange) <= maxHeight.value)){
-                scale = (scale * scaleChange).coerceIn(0.5f, 3f)
+            scale = (scale * scaleChange).coerceIn(0.85f, 3f)
+
+            // Проверяем, выходим ли за границы Parent view
+            if ((maxWidth.value * scale <= maxWidth.value) &&
+                (maxHeight.value * scale <= maxHeight.value)){
+
+                // Максимально допустимые значения для смещения
+                val maxOffsetX = maxWidth.value - maxWidth.value * scale - (-minOffsetX)
+                val maxOffsetY = maxHeight.value - maxHeight.value * scale - (-minOffsetY)
+
+                offset = Offset(
+                    x = offset.x + (offsetChange.x * scale).coerceIn(minOffsetX, maxOffsetX),
+                    y = offset.y + (offsetChange.y * scale).coerceIn(minOffsetY, maxOffsetY)
+                )
             }
-
-            // Максимально допустимые значения для смещения
-            val maxOffsetX = maxWidth.value - maxWidth.value * scale - (-minOffsetX)
-            val maxOffsetY = maxHeight.value - maxHeight.value * scale - (-minOffsetY)
-
-            offset = Offset(
-                x = offset.x + (offsetChange.x * scale).coerceIn(minOffsetX, maxOffsetX),
-                y = offset.y + (offsetChange.y * scale).coerceIn(minOffsetY, maxOffsetY)
-            )
         }
 
         Box(
@@ -61,7 +71,7 @@ fun DrawShapes(responseShapes: ResponseShapes) {
                 }
                 .fillMaxSize()
                 .onGloballyPositioned { coordinates ->
-                    val positionInRoot = coordinates.positionInRoot()
+                    val positionInRoot = coordinates.positionInParent()
                     minOffsetX = with(localDensity) { -positionInRoot.x.toDp().value }
                     minOffsetY = with(localDensity) { -positionInRoot.y.toDp().value }
                 }
