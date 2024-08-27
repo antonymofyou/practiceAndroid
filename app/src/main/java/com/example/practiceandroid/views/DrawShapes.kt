948104418936292d6688c2edc2db3f3435a29e99
Chelozzi <greyrata@mail.ru>
2024-08-27 14:22:01 +0500

    редактирование текста

--- app/src/main/java/com/example/practiceandroid/views/DrawShapes.kt
+++ app/src/main/java/com/example/practiceandroid/views/DrawShapes.kt
@@ -1,20 +1,13 @@
 package com.example.practiceandroid.views
 
-import android.util.Log
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.OverscrollEffect
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.overscroll
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,18 +15,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import com.example.practiceandroid.ext.decodeBase64ToBitmap
 import com.example.practiceandroid.models.ResponseShapes
 
 @Composable
-fun DrawShapes(responseShapes: ResponseShapes) {
+fun DrawShapes(responseShapes: ResponseShapes, focusManager: FocusManager) {
 
     var scale by remember { mutableStateOf(1f) }
 
@@ -44,7 +39,9 @@ fun DrawShapes(responseShapes: ResponseShapes) {
     var minOffsetY = 0f
 
     BoxWithConstraints (
-        modifier = Modifier.clipToBounds()
+        modifier = Modifier
+            .clipToBounds()
+            .clickable { focusManager.clearFocus() }
     ){
         val localDensity = LocalDensity.current
         val state = rememberTransformableState { scaleChange, offsetChange, _ ->
@@ -88,17 +85,20 @@ fun DrawShapes(responseShapes: ResponseShapes) {
         ) {
             // Перебираем все фигуры в списке shapes, который был передан в responseShapes
             responseShapes.shapes.forEach { shape ->
-                // Определяем тип фигуры и вызываем соответствующую функцию для ее отрисовки
-                when (shape.type) {
-                    "rectangle" -> DrawRectangle(shape)
-                    "circle" -> DrawCircle(shape)
-                    "arrow" -> DrawArrow(shape)
-                    "image" -> DrawImage(
-                        shape,
-                        image = (responseShapes.imageDictionary[shape.imageId]!!).decodeBase64ToBitmap()
-                    )
+                if (shape.type == "rectangle" && shape.text != null) {
+                    // Определяем тип фигуры и вызываем соответствующую функцию для ее отрисовки
+                    when (shape.type) {
+                        "rectangle" -> DrawRectangle(shape, focusManager)
+                        "circle" -> DrawCircle(shape)
+                        "arrow" -> DrawArrow(shape)
+                        "image" -> DrawImage(
+                            shape,
+                            image = (responseShapes.imageDictionary[shape.imageId]!!).decodeBase64ToBitmap()
+                        )
+                    }
                 }
             }
         }
     }
 }
+
