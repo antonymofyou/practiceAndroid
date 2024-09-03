e5115642cc4ba1fe9f27a595552263b04c242fba
Chelozzi <greyrata@mail.ru>
2024-09-04 02:35:33 +0500

    Частично пофиксил фокус сдвига

--- app/src/main/java/com/example/practiceandroid/views/DrawRectangle.kt
+++ app/src/main/java/com/example/practiceandroid/views/DrawRectangle.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTransformGestures
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -16,7 +14,9 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -28,9 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.boundsInParent
-import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -42,7 +40,6 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import com.example.practiceandroid.ext.valueOf
 import com.example.practiceandroid.models.ResponseShapes
-import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -54,61 +51,22 @@ fun DrawRectangle(
     focusManager: FocusManager,
     maxWidth: Float,
     maxHeight: Float,
+    helper: MutableState<Boolean>,
 ) {
     var scale by remember { mutableStateOf(1f) }
 
     var offset by remember { mutableStateOf(Offset(shape.x, shape.y)) }
 
     // Границы элемента
-    var top = offset.x
-    var left = offset.y
-    var right = left + shape.width.dp.value
-    var bottom = top + shape.height.dp.value
+    var top by remember { mutableFloatStateOf(0f) }
+    var left by remember { mutableFloatStateOf(0f) }
+    var right by remember { mutableFloatStateOf(0f) }
+    var bottom by remember { mutableFloatStateOf(0f) }
+
+    // Вспомогательная переменная для рекомпозиции
+    var recomposition = 0f
 
     val localDensity = LocalDensity.current
-    val state = rememberTransformableState { scaleChange, offsetChange, _ ->
-        // Обновляем масштаб с ограничением в пределах от 0.85f до 3f
-        scale = (scale * scaleChange).coerceIn(0.85f, 3f)
-
-        // Рассчитываем косинус и синус угла вращения в радианах
-        val rotationRadians = Math.toRadians((shape.rotation?.toDouble() ?: 0.0))
-        val cosRotation = cos(rotationRadians).toFloat()
-        val sinRotation = sin(rotationRadians).toFloat()
-
-        // MinMax значения для смещения
-        val minOffsetX = -left
-        val minOffsetY = -top
-        val maxOffsetX = maxWidth - right
-        val maxOffsetY = maxHeight - bottom
-
-        var rotatedOffsetY = offsetChange.y * scale
-        var rotatedOffsetX = offsetChange.x * scale
-
-        // Применение вращения к смещению
-        var transformedOffsetX = rotatedOffsetX * cosRotation - rotatedOffsetY * sinRotation
-        var transformedOffsetY = rotatedOffsetX * sinRotation + rotatedOffsetY * cosRotation
-
-        // Ограничение смещения по оси X
-        transformedOffsetX = if (minOffsetX > maxOffsetX) {
-            transformedOffsetX.coerceIn(maxOffsetX, minOffsetX)
-        } else {
-            transformedOffsetX.coerceIn(minOffsetX, maxOffsetX)
-        }
-
-        // Ограничение смещения по оси Y
-        transformedOffsetY = if (minOffsetY > maxOffsetY) {
-            transformedOffsetY.coerceIn(maxOffsetY, minOffsetY)
-        } else {
-            transformedOffsetY.coerceIn(minOffsetY, maxOffsetY)
-        }
-
-        // Обновление значения смещения
-        offset = Offset(
-            x = offset.x + transformedOffsetX,
-            y = offset.y + transformedOffsetY
-        )
-
-    }
 
     // Контейнер Row для размещения текста внутри прямоугольника
     Row(
@@ -135,16 +93,9 @@ fun DrawRectangle(
             .height(shape.height.dp)
             .zIndex(shape.zIndex)
             .clickable { focusManager.clearFocus() }
-            .onGloballyPositioned { layoutCoordinates  ->
-                val rect = layoutCoordinates.boundsInParent()
-                left = with(localDensity) { rect.left.toDp().value }
-                top = with(localDensity) { rect.top.toDp().value }
-                right = with(localDensity) { rect.right.toDp().value }
-                bottom = with(localDensity) { rect.bottom.toDp().value }
-            }
             .pointerInput(Unit) {
                 detectTransformGestures { _, offsetChange, scaleChange, _ ->
-
+                    helper.value = true
                     // Обновляем масштаб с ограничением в пределах от 0.85f до 3f
                     scale = (scale * scaleChange).coerceIn(0.85f, 3f)
 
@@ -185,8 +136,15 @@ fun DrawRectangle(
                         x = offset.x + transformedOffsetX,
                         y = offset.y + transformedOffsetY
                     )
-
-                }},
+                }}
+            .onGloballyPositioned { layoutCoordinates  ->
+                recomposition
+                val rect = layoutCoordinates.boundsInParent()
+                left = with(localDensity) { rect.left.toDp().value }
+                top = with(localDensity) { rect.top.toDp().value }
+                right = with(localDensity) { rect.right.toDp().value }
+                bottom = with(localDensity) { rect.bottom.toDp().value }
+            },
         verticalAlignment = Alignment.valueOf(shape.textVerticalAlignment)
     ) {
         // Перебираем текстовые блоки внутри фигуры
--- app/src/main/java/com/example/practiceandroid/views/DrawShapes.kt
+++ app/src/main/java/com/example/practiceandroid/views/DrawShapes.kt
@@ -2,6 +2,10 @@ package com.example.practiceandroid.views
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.calculateZoom
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -11,20 +15,27 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInParent
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.example.practiceandroid.ext.decodeBase64ToBitmap
 import com.example.practiceandroid.models.ResponseShapes
+import kotlinx.coroutines.awaitCancellation
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
+
 
 //Компонент для отрисовки фигур, focusManager - для сброса фокуса ввода текста
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun DrawShapes(responseShapes: ResponseShapes, focusManager: FocusManager) {
 
@@ -32,14 +43,18 @@ fun DrawShapes(responseShapes: ResponseShapes, focusManager: FocusManager) {
 
     var offset by remember { mutableStateOf(Offset.Zero) }
 
+    var helper = remember { mutableStateOf(false) }
+    var r = 1f
+    val state = rememberTransformableState { scaleChange, offsetChange, _ ->
+        // Обновляем масштаб с ограничением в пределах от 0.85f до 3f
+        scale = (scale * scaleChange).coerceIn(0.85f, 3f)
+    }
+
     BoxWithConstraints (
         modifier = Modifier
             .clipToBounds()
             .clickable { focusManager.clearFocus() }
     ){
-        val state = rememberTransformableState { scaleChange, offsetChange, _ ->
-            scale = (scale * scaleChange).coerceIn(0.85f, 3f)
-        }
 
         BoxWithConstraints(
             modifier = Modifier
@@ -52,14 +67,26 @@ fun DrawShapes(responseShapes: ResponseShapes, focusManager: FocusManager) {
                 .fillMaxSize()
                 // Добавил для отладки
                 .border(4.dp, Color.Gray)
-                .transformable(state)
                 .clipToBounds()
+                .transformable(state)
+                .pointerInput(Unit) {
+                    awaitEachGesture {
+                        if (helper.value == false){
+                            scale = r
+                        }
+                        else{
+                            helper.value = false
+                        }
+                        awaitFirstDown(pass = PointerEventPass.Initial)
+                    }
+                }
+
         ) {
             // Перебираем все фигуры в списке shapes, который был передан в responseShapes
             responseShapes.shapes.forEach { shape ->
                     // Определяем тип фигуры и вызываем соответствующую функцию для ее отрисовки
                     when (shape.type) {
-                        "rectangle" -> DrawRectangle(shape, focusManager, maxWidth.value, maxHeight.value)
+                        "rectangle" -> DrawRectangle(shape, focusManager, maxWidth.value, maxHeight.value, helper)
                         "circle" -> DrawCircle(shape, focusManager, maxWidth.value, maxHeight.value)
                         "arrow" -> DrawArrow(shape, focusManager, maxWidth.value, maxHeight.value)
                         "image" -> DrawImage(
