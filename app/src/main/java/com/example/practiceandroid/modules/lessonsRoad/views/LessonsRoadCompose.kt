5eff8c1fd77ceb0de40d227d7974fe181020a05f
DavidVista <ilyagrigorev0505@gmail.com>
2024-09-10 15:42:03 +0500

    Fix parallax effect

--- app/src/main/java/com/example/practiceandroid/modules/lessonsRoad/views/LessonsRoadCompose.kt
+++ app/src/main/java/com/example/practiceandroid/modules/lessonsRoad/views/LessonsRoadCompose.kt
@@ -4,23 +4,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.ImageShader
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.graphics.transform
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.practiceandroid.R
 import com.example.practiceandroid.modules.lessonsRoad.viewModels.LessonsRoadViewModel
+import com.example.practiceandroid.modules.lessonsRoad.viewModels.LessonsViewModel
 
 // Константы для определения типа view
 val VIEW_TYPE_RIGHT = 0
@@ -68,23 +77,29 @@ fun LessonsRoadCompose(viewType: Int) {
 
     val imageBrush = ShaderBrush(scaledShader)
 
+    // Высота столбца всех разделов
+    var columnHeight by remember {
+        mutableStateOf(0.dp)
+    }
+
+    val lessonsViewModel = viewModel<LessonsViewModel>()
+
     Box(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Box (
-            modifier = Modifier
-                .fillMaxSize()
-                .background(imageBrush)
-                .graphicsLayer {
-                    translationY = .2f * scrollState.value
-                }
-        )
         // Столбец для отображения всех разделов
         Column (
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(scrollState)
+                .background(imageBrush)
+                .graphicsLayer {
+                    translationY = .2f * scrollState.value
+                }
+                .onGloballyPositioned { coordinates ->
+                    columnHeight = lessonsViewModel.pxToDp(coordinates.size.height).dp
+                }
         ) {
             var index = 0   // общий порядковый номер уроков
             val lastIndex = lessonsRoadViewModel.groupedLessons.map { it.size }.sum() - 1  // индекс последнего урока
@@ -92,6 +107,14 @@ fun LessonsRoadCompose(viewType: Int) {
                 LessonsChapterCompose(lessonsRoadViewModel, chapter, viewType, index, lastIndex)
                 index += chapter.size
             }
+
+            // Добавочная высота для прогрузки всей дорожки.
+            // Без неё часть дорожки обрезается.
+            Box (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(columnHeight * .175f)
+            )
         }
     }
 }
